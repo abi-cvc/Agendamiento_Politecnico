4aafae3ccc1d7885bf899ee7f8aff1f52e05edc9
abi-cvc <carol.velasquez@epn.edu.ec>
2026-01-03 20:36:51 -0500

    Cambio de lugar de archivos html del front, todos se han movido a la carpeta webapp

--- src/main/java/controller/AgendarCitasController.java
+++ src/main/java/controller/AgendarCitasController.java
@@ -13,6 +13,7 @@ import java.time.LocalTime;
 
 @WebServlet("/agendarCita")
 public class AgendarCitasController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
